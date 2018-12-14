e0dfb59a17b9b9682303c8864614d7fa2a2519a8
ronan.oliveira <ronan.oliveira@asbpm.com.br>
2018-12-14 17:19:28 -0200

    correção Infos

--- src/components/Infos.js
+++ src/components/Infos.js
@@ -3,39 +3,38 @@ import PropTypes from 'prop-types';
 import { fetchAPI } from '../hocs/fetchAPI';
 import { withLoading } from '../hocs/withLoading';
 
-
 const Infos = ({ data }) => {
-    const { avatar_url, location, login, name } = data;
+  const { avatar_url, location, login, name } = data;
 
-    return (
-        <ul style={{ clear: 'both', display: 'block', listStyle: 'none' }}>
-            <li>
-                <img
-                    alt='me'
-                    src={avatar_url}
-                    style={{ margin: '0 auto' }}
-                />
-            </li>
-            <li>Meu nome: {name}</li>
-            <li>Meu username: {login}</li>
-            <li>Cidade: {location}</li>
-        </ul>
-    );
+  return (
+    <ul style={{ clear: 'both', display: 'block', listStyle: 'none' }}>
+      <li>
+        <img
+          alt='me'
+          src={avatar_url}
+          style={{ margin: '0 auto' }}
+        />
+      </li>
+      <li>Meu nome: {name}</li>
+      <li>Meu username: {login}</li>
+      <li>Cidade: {location}</li>
+    </ul>
+  )
 };
 
 Infos.defaultProps = {
-    data: {}
+  data: {}
 };
 
 Infos.propTypes = {
-    data: PropTypes.shape({
-      avatar_url: PropTypes.string,
-      location: PropTypes.string,
-      login: PropTypes.string,
-      name: PropTypes.string
-    })
-  };
-  
-  const InfosLoading = withLoading(Infos);
-  
-  export default fetchAPI(InfosLoading);
+  data: PropTypes.shape({
+    avatar_url: PropTypes.string,
+    location: PropTypes.string,
+    login: PropTypes.string,
+    name: PropTypes.string
+  })
+};
+
+const InfosLoading = withLoading(Infos);
+
+export default fetchAPI(InfosLoading);
